e993aa2c421323e7b07724119c39c8062973f2e4
mohit khade <mohitkhade11@gmail.com>
2023-09-03 19:41:27 +0530

    initial commit

---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+import chatbot from '../assets/projects/chatbot.png'
+import ecommerce_website from '../assets/projects/ecommerce_website.jpeg'
+import travel from '../assets/projects/travel.png'
+import MapClone from '../assets/projects/mapclone.png'
+
+export const data = [
+    {
+        id: 1,
+        name: "Next JS Application",
+        image: chatbot,
+        github: "https://github.com/Swappycoder/chatbot",
+        live: "",
+    },
+    {
+        id: 2,
+        name: "React JS Application",
+        image: ecommerce_website,
+        github: "https://github.com/Swappycoder/shoplifting-website",
+        live: "",
+    },
+    {
+        id: 3,
+        name: "React JS Application",
+        image: travel,
+        github: "https://github.com/Swappycoder/travel_react-app",
+        live: "",
+    },
+    {
+        id: 4,
+        name: "React JS Application",
+        image: MapClone,
+        github: "https://github.com/Swappycoder/google_map_clone",
+        live: "",
+    },
+
+
+]
